9243a62a9c44b24b97369cd7ace5182f4f9f3023
royaboya <94084201+royaboya@users.noreply.github.com>
2022-10-17 10:11:01 -0400

    fixed

--- classwork/recursive functions/palindromic.py
+++ classwork/recursive functions/palindromic.py
@@ -1,8 +1,13 @@
 def palindromic(lst):
-    if lst[0] == lst[-1]:
-        return True
-    return lst[1:] == lst[-2:]
+    if len(lst) <= 2:
+        print(lst)
+        return lst[0] == lst[-1]
+    if lst[0] != lst[-1]:
+        print(lst)
+        return False
+    return palindromic(lst[1:-1])
 
 
 print(palindromic([1, 2, 3, 4, 5]))
-print(palindromic([1,1,1,1,1]))
+print(palindromic([1, 1, 1, 1, 1]))
+print(palindromic([1, 2, 3, 2, 1]))
